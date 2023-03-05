06533ffa39e4df71b0c9a2b8ea70c85c92da422d
collin-aftermath <collin@aftermath.finance>
2023-03-04 20:48:31 -0800

    feat: create faucet class

---
+++ src/faucet/faucet.ts
@@ -0,0 +1,43 @@
+import { SignableTransaction } from "@mysten/sui.js";
+import AftermathProvider from "../aftermathProvider/aftermathProvider";
+import { SuiNetwork } from "aftermath-sdk/dist/src/config/configTypes";
+import { CoinType, ApiFaucetRequestBody } from "../types";
+
+export class Faucet extends AftermathProvider {
+	constructor(public readonly network: SuiNetwork) {
+		super(network, "faucet");
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	//// Inspections
+	/////////////////////////////////////////////////////////////////////
+
+	public async getIsPackageOnChain(): Promise<boolean> {
+		return this.fetchApi("status");
+	}
+
+	public async getSupportedCoins(): Promise<CoinType[]> {
+		return this.fetchApi("supportedCoins");
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	//// Events
+	/////////////////////////////////////////////////////////////////////
+
+	// TODO: add mint coin event getter
+
+	/////////////////////////////////////////////////////////////////////
+	//// Transactions
+	/////////////////////////////////////////////////////////////////////
+
+	public async getRequestCoinTransaction(
+		coin: CoinType
+	): Promise<SignableTransaction> {
+		return this.fetchApi<SignableTransaction, ApiFaucetRequestBody>(
+			"transactions/request",
+			{
+				coin,
+			}
+		);
+	}
+}
