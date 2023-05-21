faee281d1cb64c7dd14fa1aeb96b6ab8f432de12
collin-aftermath <collin@aftermath.finance>
2023-05-20 19:45:06 -0700

    fix: resolve coingecko price info fetching bugs

--- src/general/providers/aftermathApi.ts
+++ src/general/providers/aftermathApi.ts
@@ -63,6 +63,7 @@ export class AftermathApi {
 	) {
 		this.provider = provider;
 		this.addresses = addresses;
+		this.coinGeckoApiKey = coinGeckoApiKey;
 	}
 
 	/////////////////////////////////////////////////////////////////////
