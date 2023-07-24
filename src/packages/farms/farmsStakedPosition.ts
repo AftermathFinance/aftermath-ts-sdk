623c3c7e6e4cf7bd6a7df23f323efea169da4512
collin-aftermath <collin@aftermath.finance>
2023-07-24 12:40:47 -0700

    feat(farms): transplant staking position calculations from move to sdk

--- src/packages/farms/farmsStakedPosition.ts
+++ src/packages/farms/farmsStakedPosition.ts
@@ -11,10 +11,14 @@ import {
 	CoinType,
 	CoinsToBalance,
 	FarmsStakedPositionObject,
+	FarmsStakedPositionRewardCoin,
+	FarmsStakingPoolObject,
 	SuiNetwork,
 	Timestamp,
 	Url,
 } from "../../types";
+import { FarmsStakingPool } from "./farmsStakingPool";
+import { Fixed } from "../../general/utils/fixed";
 
 export class FarmsStakedPosition extends Caller {
 	// =========================================================================
@@ -22,7 +26,7 @@ export class FarmsStakedPosition extends Caller {
 	// =========================================================================
 
 	constructor(
-		public readonly stakedPosition: FarmsStakedPositionObject,
+		public stakedPosition: FarmsStakedPositionObject,
 		public readonly network?: SuiNetwork | Url
 	) {
 		super(network, "farms");
@@ -53,12 +57,16 @@ export class FarmsStakedPosition extends Caller {
 		);
 	};
 
-	public rewardCoinsToClaimableBalance = (): CoinsToBalance => {
+	public rewardCoinsToClaimableBalance = (inputs: {
+		stakingPool: FarmsStakingPool;
+	}): CoinsToBalance => {
 		return this.stakedPosition.rewardCoins.reduce(
 			(acc, coin) => ({
 				...acc,
-				[coin.coinType]:
-					coin.baseRewardsAccumulated - coin.baseRewardsDebt,
+				[coin.coinType]: this.rewardsEarned({
+					...inputs,
+					coinType: coin.coinType,
+				}),
 			}),
 			{} as CoinsToBalance
 		);
@@ -68,17 +76,123 @@ export class FarmsStakedPosition extends Caller {
 		return this.stakedPosition.rewardCoins.map((coin) => coin.coinType);
 	};
 
+	public rewardCoin = (inputs: { coinType: CoinType }) => {
+		const foundCoin = this.stakedPosition.rewardCoins.find(
+			(coin) => coin.coinType === inputs.coinType
+		);
+		if (!foundCoin) throw new Error("Invalid coin type");
+
+		return foundCoin;
+	};
+
 	// =========================================================================
 	//  Calculations
 	// =========================================================================
 
-	public rewardsEarned = (inputs: { coinType: CoinType }) => {
-		const rewardCoin = this.getRewardCoin(inputs.coinType);
+	public rewardsEarned = (inputs: {
+		coinType: CoinType;
+		stakingPool: FarmsStakingPool;
+	}) => {
+		const rewardCoin = this.rewardCoin(inputs);
+
+		this.updatePosition(inputs);
+
 		return rewardCoin.baseRewardsAccumulated - rewardCoin.baseRewardsDebt;
 	};
 
+	// Updates the amount of rewards that can be harvested from `self` + the position's
+	// debt. If the position's lock duration has elapsed, it will be unlocked.
+	public updatePosition = (inputs: { stakingPool: FarmsStakingPool }) => {
+		// i. Increase the vault's `rewardsAccumulatedPerShare` values.
+		inputs.stakingPool.emitRewards();
+
+		for (const [
+			rewardCoinIndex,
+			rewardCoin,
+		] of inputs.stakingPool.stakingPool.rewardCoins.entries()) {
+			//******************************************************************************************//
+			//                      debt (i.e. total_rewards_from_time_t0_to_th-1)                      //
+			// .--- pending_rewards_at_time_th_minus_1 ---|                                             //
+			// |------------------------------------------+-------------------------------------------| //
+			// t0                                        th-1                                       now //
+			// '----------------------------- total_rewards_from_time_t0 -----------------------------' //
+			//******************************************************************************************//
+
+			// NOTE: `total_rewards_from_time_t0` contains the amount of rewards a position would receive
+			//  -- from time t0 to the current time -- given the vault's current state when, in reality,
+			//  we need to calculate just the rewards that have accumulated since the last time the
+			//  position's `rewards_accumulated` was updated [labeled time th-1].
+			//
+			const [
+				totalBaseRewardsFromTimeT0,
+				totalMultiplierRewardsFromTimeT0,
+			] = this.calcTotalRewardsFromTimeT0(rewardCoin);
+
+			// NOTE: new reward types might have been added to the vault since this position last called
+			//  `update_ pending_rewards`, so we need to be cautious when borrowing from `rewards_debt`
+			//  and `rewards_accumulated`.
+			//
+			if (rewardCoinIndex >= this.stakedPosition.rewardCoins.length) {
+				this.stakedPosition.rewardCoins.push({
+					coinType: rewardCoin.coinType,
+					baseRewardsAccumulated: BigInt(0),
+					baseRewardsDebt: BigInt(0),
+					multiplierRewardsAccumulated: BigInt(0),
+					multiplierRewardsDebt: BigInt(0),
+				});
+			}
+			const stakedPositionRewardCoin =
+				this.stakedPosition.rewardCoins[rewardCoinIndex];
+
+			// NOTE: Every time a position's `rewards_accumulated` is updated, a snapshot of the total
+			//  rewards received from time t0 is taken and stored as the position's `debt` field. Here,
+			//  the debt is subtracted from `total_rewards_from_time_t0` in order to calculate the amount
+			//  of rewards that have been accumulated since the last time `rewards_accumulated` was
+			//  updated.
+			//
+			//  `pending_rewards_at_time_th_minus_1` is added to the resulting value to account for all
+			//  rewards that had accumulated up until the last time `update_position` was updated.
+			//
+			// iia. Allocate rewards to this position that have been emitted since the last time this
+			//  function was called.
+			this.stakedPosition.rewardCoins[
+				rewardCoinIndex
+			].baseRewardsAccumulated =
+				totalBaseRewardsFromTimeT0 -
+				stakedPositionRewardCoin.baseRewardsDebt +
+				stakedPositionRewardCoin.baseRewardsAccumulated;
+
+			this.stakedPosition.rewardCoins[
+				rewardCoinIndex
+			].multiplierRewardsAccumulated =
+				totalMultiplierRewardsFromTimeT0 -
+				stakedPositionRewardCoin.multiplierRewardsDebt +
+				stakedPositionRewardCoin.multiplierRewardsAccumulated;
+
+			// iib. Set the position's current debt to the total rewards attributed to this position given
+			//  the vault's current state. This allows the next `update_position` call to disregard
+			//  all rewards accumulated up until this point -- preventing double emission cases.
+			this.stakedPosition.rewardCoins[rewardCoinIndex].baseRewardsDebt =
+				totalBaseRewardsFromTimeT0;
+			this.stakedPosition.rewardCoins[
+				rewardCoinIndex
+			].multiplierRewardsDebt = totalMultiplierRewardsFromTimeT0;
+		}
+
+		const currentTimestamp = Date.now();
+
+		// iii. Remove the position's lock multiplier + bonus staked amount if the position is no
+		//  longer locked.
+		if (this.unlockTimestamp() < currentTimestamp) {
+			// TODO: handle unlock
+			// unlock(self, vault, clock);
+		}
+
+		this.stakedPosition.lastHarvestRewardsTimestamp = currentTimestamp;
+	};
+
 	public rewardsApy = (inputs: { coinType: CoinType }) => {
-		const rewardCoin = this.getRewardCoin(inputs.coinType);
+		const rewardCoin = this.rewardCoin(inputs);
 
 		// TODO: make this calculation
 
@@ -188,15 +302,75 @@ export class FarmsStakedPosition extends Caller {
 	// =========================================================================
 
 	// =========================================================================
-	//  Helpers
+	//  Calculations
 	// =========================================================================
 
-	private getRewardCoin = (coinType: CoinType) => {
-		const foundCoin = this.stakedPosition.rewardCoins.find(
-			(coin) => coin.coinType === coinType
-		);
-		if (!foundCoin) throw new Error("Invalid coin type");
+	// Calculates a position's accrued rewards [from time t0] given a vault's
+	//  `rewardsAccumulatedPerShare`. If the position is beyond its lock duration, we need to only
+	//  apply the lock multiplier rewards to the time spent locked.
+	public calcTotalRewardsFromTimeT0(inputs: {
+		rewardsAccumulatedPerShare: Balance;
+	}): [Balance, Balance] {
+		const { rewardsAccumulatedPerShare } = inputs;
 
-		return foundCoin;
-	};
+		const currentTimestamp = Date.now();
+		const lockEndTimestamp = this.unlockTimestamp();
+		const lastRewardTimestamp =
+			this.stakedPosition.lastHarvestRewardsTimestamp;
+
+		const principalStakedAmount = this.stakedPosition.stakedAmount;
+		// Base [e.g. principal] staked amount receives full (unaltered) rewards.
+		const rewardsAttributedToPrincipal =
+			(principalStakedAmount * rewardsAccumulatedPerShare) /
+			Fixed.fixedOneB;
+
+		// The position should only receive multiplied rewards for the time that was spent locked since
+		//  the last harvest. This case occurs when the user calls `pending_rewards` after the position's
+		//  lock duration has expired.
+		const rewardsAttributedToLockMultiplier = () => {
+			return currentTimestamp <= lockEndTimestamp
+				? (this.stakedPosition.stakedAmountWithMultiplier *
+						rewardsAccumulatedPerShare) /
+						Fixed.fixedOneB
+				: lockEndTimestamp <= lastRewardTimestamp
+				? // Short circuit in the case the position hasn't been locked since the last harvest. Also
+				  //  required to not error on `lockEndTimestamp - lastRewardTimestamp`.
+				  BigInt(0)
+				: (() => {
+						// Multiplier staked amount receives (altered) rewards dependent on the total time the
+						//  position was locked since the last harvest.
+						const totalRewardsAttributedToLockMultiplier =
+							(this.stakedPosition.stakedAmountWithMultiplier *
+								rewardsAccumulatedPerShare) /
+							Fixed.fixedOneB;
+
+						const timeSinceLastHarvest =
+							currentTimestamp - lastRewardTimestamp;
+						const timeSpentLockedSinceLastHarvestMs =
+							lockEndTimestamp - lastRewardTimestamp;
+
+						// ********************************************************************************************//
+						//  / timeSpentLockedSinceLastHarvestMs \                                                //
+						// | ----------------------------------------- | x totalRewardsAttributedToLockMultiplier //
+						//  \       timeSinceLastHarvest        /                                                //
+						// ********************************************************************************************//
+
+						// Only disperse the multiplied rewards that were received while this position was locked.
+						return (
+							(totalRewardsAttributedToLockMultiplier *
+								BigInt(
+									Math.floor(
+										timeSpentLockedSinceLastHarvestMs
+									)
+								)) /
+							BigInt(Math.floor(timeSinceLastHarvest))
+						);
+				  })();
+		};
+
+		return [
+			rewardsAttributedToPrincipal,
+			rewardsAttributedToLockMultiplier(),
+		];
+	}
 }
--- src/packages/farms/farmsStakingPool.ts
+++ src/packages/farms/farmsStakingPool.ts
@@ -9,6 +9,7 @@ import {
 	FarmsStakingPoolObject,
 } from "./farmsTypes";
 import { ObjectId, SuiAddress } from "@mysten/sui.js";
+import { Helpers } from "../../general/utils";
 
 export class FarmsStakingPool extends Caller {
 	// =========================================================================
@@ -16,7 +17,7 @@ export class FarmsStakingPool extends Caller {
 	// =========================================================================
 
 	constructor(
-		public readonly stakingPool: FarmsStakingPoolObject,
+		public stakingPool: FarmsStakingPoolObject,
 		public readonly network?: SuiNetwork | Url
 	) {
 		super(network, "farms");
@@ -33,6 +34,60 @@ export class FarmsStakingPool extends Caller {
 
 	public rewardCoinTypes = (): CoinType[] => {
 		return this.stakingPool.rewardCoins.map((coin) => coin.coinType);
+	};
+
+	public rewardCoin = (inputs: { coinType: CoinType }) => {
+		const foundCoin = this.stakingPool.rewardCoins.find(
+			(coin) => coin.coinType === inputs.coinType
+		);
+		if (!foundCoin) throw new Error("Invalid coin type");
+
+		return foundCoin;
+	};
+
+	// =========================================================================
+	//  Calculations
+	// =========================================================================
+
+	// Calculates the amount of rewards that have emitted since the last time this function has been
+	// called. Updates `rewards_accumulated_per_share`.
+	public emitRewards = () => {
+		const currentTimestamp = Date.now();
+
+		// ia. Check that the vault has deposits.
+		if (this.stakingPool.stakedAmount === BigInt(0)) return;
+
+		const rewardCoins = Helpers.deepCopy(
+			this.stakingPool.rewardCoins.entries()
+		);
+		for (const [rewardCoinIndex, rewardCoin] of rewardCoins) {
+			// ib. Check that enough time has passed since the last emission.
+			if (
+				currentTimestamp <
+				rewardCoin.lastRewardTimestamp + rewardCoin.emissionSchedulesMs
+			)
+				continue;
+
+			// ii. Calculate how many rewards have to be emitted.
+			const number_of_emissions =
+				(currentTimestamp - rewardCoin.lastRewardTimestamp) /
+				rewardCoin.emissionSchedulesMs;
+
+			const rewardsToEmit =
+				rewardCoin.emissionRate *
+				BigInt(Math.floor(number_of_emissions));
+
+			// iii. Increase the amount of rewards emitted per share.
+			this.increaseRewardsAccumulatedPerShare({
+				rewardsToEmit,
+				rewardCoinIndex,
+			});
+
+			// iv. Update reward's `last_reward_timestamp`.
+			this.stakingPool.rewardCoins[rewardCoinIndex].lastRewardTimestamp =
+				rewardCoin.lastRewardTimestamp +
+				number_of_emissions * rewardCoin.emissionSchedulesMs;
+		}
 	};
 
 	// =========================================================================
@@ -135,5 +190,37 @@ export class FarmsStakingPool extends Caller {
 				stakingPoolId: this.stakingPool.objectId,
 			}
 		);
+	}
+
+	// =========================================================================
+	//  Private
+	// =========================================================================
+
+	// =========================================================================
+	//  Calculations
+	// =========================================================================
+
+	private increaseRewardsAccumulatedPerShare(inputs: {
+		rewardsToEmit: Balance;
+		rewardCoinIndex: number;
+	}) {
+		const { rewardsToEmit, rewardCoinIndex } = inputs;
+
+		// i. Calculate the pro-rata amount of rewards allocated to each staked amount.
+		const newRewardsAccumulatedPerShare =
+			(rewardsToEmit * BigInt(1_000_000_000_000_000_000)) /
+			this.stakingPool.stakedAmountWithMultiplier;
+
+		if (newRewardsAccumulatedPerShare === BigInt(0)) return;
+
+		// ii. Increase the amount of rewards emitted per share.
+		const rewards_accumulated_per_share =
+			this.stakingPool.rewardCoins[rewardCoinIndex]
+				.rewardsAccumulatedPerShare;
+
+		this.stakingPool.rewardCoins[
+			rewardCoinIndex
+		].rewardsAccumulatedPerShare =
+			rewards_accumulated_per_share + newRewardsAccumulatedPerShare;
 	}
 }
