ecbe5c06fbadd1d4a210a257442636da2b4a51d7
collin-aftermath <collin@aftermath.finance>
2023-09-03 12:09:42 -0700

    feat(perpetuals): create pyth price feeds api class

---
+++ src/general/priceFeeds/priceFeedsApi.ts
@@ -0,0 +1,37 @@
+import { Url } from "../../types";
+import { SuiPriceServiceConnection } from "@pythnetwork/pyth-sui-js";
+import { PythPriceFeedId } from "./priceFeedsTypes";
+
+export class PriceFeedsApi {
+	// =========================================================================
+	//  Class Members
+	// =========================================================================
+
+	protected readonly connection: SuiPriceServiceConnection;
+
+	// =========================================================================
+	//  Constructor
+	// =========================================================================
+
+	constructor(priceServiceEndpoint: Url) {
+		this.connection = new SuiPriceServiceConnection(priceServiceEndpoint);
+	}
+
+	// =========================================================================
+	//  Public Methods
+	// =========================================================================
+
+	public fetchPrices = async (
+		priceFeedIds: PythPriceFeedId[]
+	): Promise<number[]> => {
+		const priceFeeds = await this.connection.getLatestPriceFeeds(
+			priceFeedIds
+		);
+		if (priceFeeds === undefined)
+			throw new Error("unable to fetch pyth price feeds");
+
+		return priceFeeds?.map((feed) =>
+			feed.getPriceUnchecked().getPriceAsNumberUnchecked()
+		);
+	};
+}
