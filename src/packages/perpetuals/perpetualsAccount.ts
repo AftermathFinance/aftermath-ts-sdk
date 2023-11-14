7660e8fc07771956cf418ec08b801f4fd8663159
matteo-aftermath <matteo@aftermath.finance>
2023-11-14 16:23:42 +0100

    Fixed free Collateral calculation

--- src/packages/perpetuals/perpetualsAccount.ts
+++ src/packages/perpetuals/perpetualsAccount.ts
@@ -240,8 +240,31 @@ export class PerpetualsAccount extends Caller {
 	}): number => {
 		const totalFunding = this.calcUnrealizedFundingsForAccount(inputs);
 
-		const { totalPnL, totalMinInitialMargin } =
-			this.calcPnLAndMarginForAccount(inputs);
+		// const { totalPnL, totalMinInitialMargin } =
+		// 	this.calcPnLAndMarginForAccount(inputs);
+
+		const zipped = Helpers.zip(inputs.markets, inputs.indexPrices);
+		let totalPnL = 0;
+		let totalMinInitialMargin = 0;
+		let totalMinMaintenanceMargin = 0;
+		let totalNetAbsBaseValue = 0;
+
+		zipped.forEach(([market, indexPrice]) => {
+			const {
+				pnl,
+				minInitialMargin,
+				minMaintenanceMargin,
+				netAbsBaseValue,
+			} = this.calcPnLAndMarginForPosition({
+				market,
+				indexPrice,
+			});
+
+			if (pnl < 0) totalPnL += pnl;
+			totalMinInitialMargin += minInitialMargin;
+			totalMinMaintenanceMargin += minMaintenanceMargin;
+			totalNetAbsBaseValue += netAbsBaseValue;
+		});
 
 		let collateralUsd =
 			IFixedUtils.numberFromIFixed(this.account.collateral) *
