a9282648cd99b0737bc1e368ace0210e91502d74
collin-aftermath <collin@aftermath.finance>
2024-04-08 17:46:36 -0700

    chore: update exports

--- src/packages/index.ts
+++ src/packages/index.ts
@@ -5,6 +5,7 @@ export * from "./staking";
 export * from "./leveragedStaking";
 export * from "./faucet";
 export * from "./nftAmm";
+export * from "./fractionalNfts";
 export * from "./referralVault";
 export * from "./farms";
 export * from "./perpetuals";
