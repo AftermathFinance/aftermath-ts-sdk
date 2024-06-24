873eba4cb78328014c48c4371af8d6a391e5008c
patison5 <patison4@yandex.ru>
2024-06-24 14:47:09 +0300

    chore: added dca.ts documentation, removed unnecessary imports

--- src/packages/dca/dca.ts
+++ src/packages/dca/dca.ts
@@ -1,10 +1,10 @@
 
-import { EventsInputs, SuiNetwork, Url, ObjectId, AnyObjectType } from "../../types";
+import { SuiNetwork } from "../../types";
 import { Caller } from "../../general/utils/caller";
 import { AftermathApi } from "../../general/providers";
 
 import { SuiAddress } from "../../types";
-import { ApiDCAsOwnedBody, ApiDcaCancelOrderBody, ApiDcaInitializeOrderBody, DcaOrdersOjbect } from "./dcaTypes";
+import { ApiDCAsOwnedBody, ApiDcaCancelOrderBody, ApiDcaInitializeOrderBody, DcaOrdersObject } from "./dcaTypes";
 
 export class Dca extends Caller {
 	
@@ -23,31 +23,64 @@ export class Dca extends Caller {
 	//  Class Objects
 	// =========================================================================
 
+	/**
+	 * Fetches the API for dollar cost averaging orders list.
+	 * @async
+	 * @param { ApiDCAsOwnedBody } inputs - An object containing the walletAddress.
+	 * @returns {Promise<DcaOrdersObject>} A promise that resolves to object with array of fetched events for active and past dca's.
+	 */
+
+	public async getDcaOrdersObject(inputs: ApiDCAsOwnedBody) {
+		return this.fetchApi<DcaOrdersObject, ApiDCAsOwnedBody>("", inputs);
+	}
+
+	/**
+	 * Fetches the API for dollar cost averaging active orders list.
+	 * @async
+	 * @param { ApiDCAsOwnedBody } inputs - An object containing the walletAddress.
+	 * @returns {Promise<DcaOrdersObject>} A promise that resolves to object with array of fetched events for active and past dca's.
+	 */
+
     public async getActiveDcaOrders(inputs: {
         walletAddress: SuiAddress;
     }) {
-        return inputs;
+         return this.fetchApi<DcaOrdersObject, ApiDCAsOwnedBody>("owned-active-orders", inputs);
     }
+
+	/**
+	 * Fetches the API for dollar cost averaging past orders list.
+	 * @async
+	 * @param { ApiDCAsOwnedBody } inputs - An object containing the walletAddress.
+	 * @returns {Promise<DcaOrdersObject>} A promise that resolves to object with array of fetched events for active and past dca's.
+	 */
 
     public async getPastDcaOrders(inputs: {
         walletAddress: SuiAddress;
     }) {
-        return inputs;
+        return this.fetchApi<DcaOrdersObject, ApiDCAsOwnedBody>("owned-past-orders", inputs);
     }
-
-	public async getDcaOrdersObject(inputs: ApiDCAsOwnedBody) {
-		return this.fetchApi<DcaOrdersOjbect, ApiDCAsOwnedBody>("", inputs);
-	}
 
 	// =========================================================================
 	// Transactions
 	// =========================================================================
+
+	/**
+	 * Fetches the API transaction for creating DCA order.
+	 * @param inputs - The inputs for the transaction.
+	 * @returns A promise that resolves with the API transaction.
+	 */
 
     public async getCreateDcaOrderTx(
 		inputs: ApiDcaInitializeOrderBody
 	) {
 		return this.useProvider().fetchBuildCreateOrderTx(inputs);
 	}
+
+	/**
+	 * Fetches the API transaction for canceling DCA order.
+	 * @param inputs - The inputs for the transaction.
+	 * @returns A promise that resolves with the API transaction.
+	 */
 
 	public async getCancelDcaOrderTx(
 		inputs: ApiDcaCancelOrderBody
