69cb29a3d4c60ee6982ba5d328c16f60e5d9ed5c
Fedor-Aftermath <fedor@aftermath.finance>
2024-11-28 13:25:53 +0200

    chore: removed key from user api

--- src/packages/userData/userDataTypes.ts
+++ src/packages/userData/userDataTypes.ts
@@ -8,16 +8,12 @@ import { SuiAddress } from "../../types";
 // User Fetch
 // =========================================================================
 
-export type UserDataKeyType = "dca" | "limit";
-
 export interface ApiUserDataCreateUserBody {
-	key: UserDataKeyType;
 	walletAddress: SuiAddress;
 	bytes: string;
 	signature: string;
 }
 
 export interface ApiUserDataOwnedBody {
-	key: UserDataKeyType;
 	walletAddress: SuiAddress;
 }
