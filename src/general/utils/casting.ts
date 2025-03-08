6da75589251c7fc85b7d3a24f7920d186ad15b77
Fedor-Aftermath <fedor@aftermath.finance>
2025-03-08 18:49:22 +0200

    chore: removed all fe logic from sdk

--- src/general/utils/casting.ts
+++ src/general/utils/casting.ts
@@ -16,8 +16,6 @@ import { BcsTypeName } from "../types/castingTypes";
 import { SuiObjectResponse } from "@mysten/sui/client";
 import { NftsApiCasting } from "../nfts/nftsApiCasting";
 import { OracleApiCasting } from "../../packages/oracle/api/oracleApiCasting";
-import { DcaApiCasting } from "../../packages/dca/api/dcaApiCasting";
-import { LimitOrdersApiCasting } from "../../packages/limitOrders/api/limitOrdersApiCasting";
 /**
  * Utility class for casting and conversion functions.
  * @class
@@ -38,8 +36,6 @@ export class Casting {
 	public static oracle = OracleApiCasting;
 	public static farms = FarmsApiCasting;
 	public static nfts = NftsApiCasting;
-	public static dca = DcaApiCasting;
-	public static limitOrders = LimitOrdersApiCasting;
 
 	// =========================================================================
 	//  Constants
