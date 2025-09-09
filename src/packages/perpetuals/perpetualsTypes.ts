fb2b27d5ea243dc0339a85819315e3c260da5c28
collin <collin@aftermath.finance>
2025-09-09 10:05:00 -0700

    refactor(perps): misc updates for final perps contract changes

--- src/packages/perpetuals/perpetualsTypes.ts
+++ src/packages/perpetuals/perpetualsTypes.ts
@@ -117,7 +117,6 @@ export interface PerpetualsSubAccount {
 	accountId: PerpetualsAccountId;
 	collateralCoinType: CoinType;
 	collateral: IFixed;
-	users: SuiAddress[];
 	objectVersion: ObjectVersion;
 	// objectDigest: ObjectDigest;
 	objectId: ObjectId;
@@ -139,6 +138,7 @@ export interface PerpetualsMarketParams {
 	premiumTwapPeriodMs: bigint;
 	spreadTwapFrequencyMs: bigint;
 	spreadTwapPeriodMs: bigint;
+	gasPriceTwapPeriodMs: bigint;
 	makerFee: IFixed;
 	takerFee: IFixed;
 	liquidationFee: IFixed;
@@ -147,7 +147,9 @@ export interface PerpetualsMarketParams {
 	minOrderUsdValue: IFixed;
 	lotSize: bigint;
 	tickSize: bigint;
-	liquidationTolerance: bigint;
+	scaling_factor: IFixed;
+	gas_price_taker_fee: IFixed;
+	z_score_threshold: IFixed;
 	maxPendingOrders: bigint;
 	baseOracleTolerance: bigint;
 	collateralOracleTolerance: bigint;
@@ -497,17 +499,15 @@ export interface CreatedAccountEvent extends Event {
 	accountId: PerpetualsAccountId;
 }
 
-export interface CreatedSubAccountEvent extends Event {
-	users: SuiAddress[];
-	accountId: PerpetualsAccountId;
-	subAccountId: ObjectId;
-}
+// export interface CreatedSubAccountEvent extends Event {
+// 	accountId: PerpetualsAccountId;
+// 	subAccountId: ObjectId;
+// }
 
-export interface SetSubAccountUsersEvent extends Event {
-	users: SuiAddress[];
-	accountId: PerpetualsAccountId;
-	subAccountId: ObjectId;
-}
+// export interface SetSubAccountUsersEvent extends Event {
+// 	accountId: PerpetualsAccountId;
+// 	subAccountId: ObjectId;
+// }
 
 export interface SetPositionInitialMarginRatioEvent extends Event {
 	marketId: PerpetualsMarketId;
