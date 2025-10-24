b2aa8977a14cbe4498cc8af68c64a8249ab4892b
collin <collin@aftermath.finance>
2025-10-24 14:06:04 -0700

    feat(referrals): add all referral endpoints

--- src/packages/referrals/referrals.ts
+++ src/packages/referrals/referrals.ts
@@ -2,8 +2,15 @@ import { Caller } from "../../general/utils/caller";
 import {
 	ApiReferralsCreateReferralLinkBody,
 	ApiReferralsCreateReferralLinkResponse,
+	ApiReferralsGetRefereesBody,
+	ApiReferralsGetRefereesResponse,
+	ApiReferralsGetRefCodeBody,
+	ApiReferralsGetRefCodeResponse,
+	ApiReferralsSetReferrerBody,
+	ApiReferralsSetReferrerResponse,
 	CallerConfig,
-	SuiAddress,
+	ApiReferralsIsRefCodeTakenBody,
+	ApiReferralsIsRefCodeTakenResponse,
 } from "../../types";
 
 export class Referrals extends Caller {
@@ -22,12 +29,56 @@ export class Referrals extends Caller {
 	}
 
 	// =========================================================================
-	//  Inspections
+	//  Fetching
+	// =========================================================================
+
+	public async getRefCode(
+		inputs: ApiReferralsGetRefCodeBody
+	): Promise<ApiReferralsGetRefCodeResponse> {
+		return this.fetchApi("ref-code", inputs);
+	}
+
+	public async getReferees(
+		inputs: ApiReferralsGetRefereesBody
+	): Promise<ApiReferralsGetRefereesResponse> {
+		return this.fetchApi("referees", inputs);
+	}
+
+	public async isRefCodeTaken(
+		inputs: ApiReferralsIsRefCodeTakenBody
+	): Promise<ApiReferralsIsRefCodeTakenResponse> {
+		return this.fetchApi("is-ref-code-taken", inputs);
+	}
+
+	// =========================================================================
+	//  Actions
 	// =========================================================================
 
 	public async createReferralLink(
 		inputs: ApiReferralsCreateReferralLinkBody
 	): Promise<ApiReferralsCreateReferralLinkResponse> {
-		return this.fetchApi("create", inputs);
+		return this.fetchApi("create-referral-link", inputs);
+	}
+
+	public async setReferrer(
+		inputs: ApiReferralsSetReferrerBody
+	): Promise<ApiReferralsSetReferrerResponse> {
+		return this.fetchApi("set-referrer", inputs);
+	}
+
+	// =========================================================================
+	//  Messages to Sign
+	// =========================================================================
+
+	public createReferralLinkMessageToSign() {
+		return {
+			action: "CREATE_REFERRAL_LINK",
+		};
+	}
+
+	public setReferrerMessageToSign() {
+		return {
+			action: "SET_REFERRER",
+		};
 	}
 }
--- src/packages/referrals/referralsTypes.ts
+++ src/packages/referrals/referralsTypes.ts
@@ -1,5 +1,74 @@
 import { SuiAddress, Timestamp } from "../../types";
 
+// =========================================================================
+//  Data
+// =========================================================================
+
+export interface ReferralsRefereeInfo {
+	/**
+	 *  The wallet address of the referee
+	 */
+	walletAddress: SuiAddress;
+	/**
+	 *  When the referee joined via this ref link
+	 */
+	joinedAt: Timestamp;
+}
+
+// =========================================================================
+//  API
+// =========================================================================
+
+export interface ApiReferralsGetRefCodeBody {
+	/**
+	 *  The wallet address to get referral code of
+	 */
+	walletAddress: SuiAddress;
+}
+
+export interface ApiReferralsGetRefCodeResponse {
+	/**
+	 * The referral code of queried wallet address
+	 */
+	refCode: string;
+}
+
+export interface ApiReferralsGetRefereesBody {
+	/**
+	 * Ref code to get referees for
+	 */
+	refCode: string;
+}
+
+export interface ApiReferralsGetRefereesResponse {
+	/**
+	 * The referral code queried
+	 */
+	refCode: string;
+	/**
+	 * The referrer's wallet address
+	 */
+	referrerAddress: SuiAddress;
+	/**
+	 * List of referees
+	 */
+	referees: ReferralsRefereeInfo[];
+}
+
+export interface ApiReferralsIsRefCodeTakenBody {
+	/**
+	 * The referral code queried if taken
+	 */
+	refCode: string;
+}
+
+export interface ApiReferralsIsRefCodeTakenResponse {
+	/**
+	 * True if the ref code is already claimed by a user
+	 */
+	isTaken: boolean;
+}
+
 export interface ApiReferralsCreateReferralLinkBody {
 	/**
 	 * The wallet address of the user creating the ref link. Required for authentication.
@@ -27,13 +96,51 @@ export interface ApiReferralsCreateReferralLinkResponse {
 	/**
 	 * The wallet address of the referrer
 	 */
-	walletAddress: string;
+	walletAddress: SuiAddress;
 	/**
 	 * Timestamp when the ref link was created
 	 */
 	createdAt: Timestamp;
 	/**
 	 * Status of the creation
+	 */
+	status: string;
+}
+
+export interface ApiReferralsSetReferrerBody {
+	/**
+	 * The wallet address of the referee. Required for authentication.
+	 */
+	walletAddress: SuiAddress;
+	/**
+	 * The bytes of the message signed by the referee's wallet. Required for authentication.
+	 */
+	bytes: string;
+	/**
+	 * The signature of the message signed by the referee's wallet. Required for authentication.
+	 */
+	signature: string;
+	/**
+	 * Ref code for referral to link
+	 */
+	refCode: string;
+}
+
+export interface ApiReferralsSetReferrerResponse {
+	/**
+	 * The wallet address of the referee
+	 */
+	refereeAddress: SuiAddress;
+	/**
+	 * The referral code used
+	 */
+	refCode: string;
+	/**
+	 * Timestamp when the referral relationship was established
+	 */
+	createdAt: Timestamp;
+	/**
+	 * Status of the operation
 	 */
 	status: string;
 }
