d05f47babd9a1828c8e4d73f7447bb4680abd8b8
Konstantin Barabanov <criitch@yandex.ru>
2025-11-15 18:35:50 +0300

    fix: jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  transform: {'^.+\\.ts?$': 'ts-jest'},
+export default {
+  transform: { '^.+\\.ts?$': 'ts-jest' },
   testEnvironment: 'node',
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
@@ -8,7 +8,5 @@ module.exports = {
     "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$",
     "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$",
     "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$",
-],
+  ],
 };
-
-export {}
